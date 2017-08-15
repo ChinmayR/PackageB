adc166de43fcdd7caefea5477d13d0bc7eaab508
ChinmayR <chinmayr@uber.com>
2017-08-15 14:27:37 -0700

    Made change to handle D's backwards incompatible change

--- provider.go
+++ provider.go
@@ -3,5 +3,6 @@ package PackageB
 import "github.com/ChinmayR/PackageD"
 
 func FuncInPackageB() string {
-	return "From PackageB: " + PackageD.FuncInPackageD()
+	dString, _ := PackageD.FuncInPackageD()
+	return "From PackageB: " + dString
 }
