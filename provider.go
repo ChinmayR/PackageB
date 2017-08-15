bc5bb8f833e2d02f0399f389a2f2d9a8cfbe3d7d
ChinmayR <chinmayr@uber.com>
2017-08-15 15:16:44 -0700

    Revert "Made change to handle D's backwards incompatible change"
    This reverts commit adc166de43fcdd7caefea5477d13d0bc7eaab508.

--- provider.go
+++ provider.go
@@ -3,6 +3,5 @@ package PackageB
 import "github.com/ChinmayR/PackageD"
 
 func FuncInPackageB() string {
-	dString, _ := PackageD.FuncInPackageD()
-	return "From PackageB: " + dString
+	return "From PackageB: " + PackageD.FuncInPackageD()
 }
