54db2381795e371a4b3e7c9489c19065ac425340
ChinmayR <chinmayr@uber.com>
2017-08-16 14:37:33 -0700

    Pushing dep artifacts

--- provider.go
+++ provider.go
@@ -3,5 +3,8 @@ package PackageB
 import "github.com/ChinmayR/PackageD"
 
 func FuncInPackageB() string {
-	return "From PackageB: " + PackageD.FuncInPackageD()
+	dOut, _ := PackageD.FuncInPackageD()
+	return "From PackageB: " + dOut
 }
+
+
