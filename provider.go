c5dbf427bda0772f96f7c1669fe143887cb10f79
ChinmayR <chinmayr@uber.com>
2017-08-24 13:48:02 -0700

    fixed pack b

--- provider.go
+++ provider.go
@@ -3,5 +3,6 @@ package PackageB
 import "github.com/ChinmayR/PackageD"
 
 func FuncInPackageB() string {
-	return "From PackageB: " + PackageD.FuncInPackageD()
+	str, _ := PackageD.FuncInPackageD()
+	return "From PackageB: " + str
 }
